41d2562c5869bec5e1c4daa44d17da162f86df9a
Han Chung <105803853+SevenLims@users.noreply.github.com>
2024-07-11 15:52:50 +0800

    Add files via upload

--- assignment_db.sql
+++ assignment_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2024 at 03:56 PM
+-- Generation Time: Jul 11, 2024 at 09:49 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -94,14 +94,14 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `description`, `price`, `rrp`, `quantity`, `image_name`, `date_created`, `submittedby`) VALUES
-(1, 'Luxury Smart Watch', '<p>Unique watch made with stainless steel, ideal for those that prefer interactive watches.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Powered by Android with built-in apps.</li>\r\n<li>Adjustable to fit most.</li>\r\n<li>Long battery life, continuous wear for up to 2 days.</li>\r\n<li>Lightweight design, comfort on your wrist.</li>\r\n</ul>', 79.99, 109.99, 10, 'luxsmartwatch.jpg', '2024-03-27 13:01:16', 'Vincent'),
-(2, 'Headphone', '<p>Immersive sound experience with our latest headphones, perfect for music enthusiasts.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Crystal clear sound quality.</li>\r\n<li>Ergonomic design for long-lasting comfort.</li>\r\n<li>Compatible with all devices.</li>\r\n<li>Adjustable headband for a personalized fit.</li>\r\n</ul>', 49.99, 0, 20, 'headphone.jpg', '2024-03-27 13:13:52', 'Vincent'),
+(1, 'Luxury Smart Watch', '<p>Unique watch made with stainless steel, ideal for those that prefer interactive watches.</p><h3>Features</h3><ul><li>Powered by Android with built-in apps.</li><li>Adjustable to fit most.</li><li>Long battery life, continuous wear for up to 2 days.</li><li>Lightweight design, comfort on your wrist.</li></ul>', 79.99, 109.99, 20, 'luxsmartwatch.jpg', '2024-04-08 18:22:22', 'Vincent'),
+(2, 'Headphone', '<p>Immersive sound experience with our latest headphones, perfect for music enthusiasts.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Crystal clear sound quality.</li>\r\n<li>Ergonomic design for long-lasting comfort.</li>\r\n<li>Compatible with all devices.</li>\r\n<li>Adjustable headband for a personalized fit.</li>\r\n</ul>', 49.99, 0.00, 20, 'headphone.jpg', '2024-03-27 13:13:52', 'Vincent'),
 (3, 'Digital Camera', '<p>Capture life\'s moments in stunning detail with our high-resolution digital camera.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Professional-grade image quality.</li>\r\n<li>Multiple shooting modes for versatility.</li>\r\n<li>Compact and lightweight design.</li>\r\n<li>Easy sharing with built-in Wi-Fi.</li>\r\n</ul>', 349.99, 449.99, 5, 'camera.jpg', '2024-03-27 13:21:02', 'Vincent'),
 (4, 'Phone Case', '<p>Protect your phone in style with our durable and fashionable phone cases.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Shock-absorbent materials.</li>\r\n<li>Precision cutouts for easy access to ports and buttons.</li>\r\n<li>Slim and lightweight design.</li>\r\n</ul>', 36.99, 59.99, 30, 'phonecase.jpg', '2024-03-27 13:11:46', 'Vincent'),
 (5, 'Adapter', '<p>Stay connected wherever you go with our versatile universal adapter.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Compatible with multiple plug types.</li>\r\n<li>Compact and portable for travel.</li>\r\n<li>Built-in surge protection.</li>\r\n<li>Sturdy construction for durability.</li>\r\n</ul>', 19.99, 29.99, 35, 'adapter.jpg', '2024-03-27 13:23:56', 'Vincent'),
-(6, 'USB-C Cable', '<p>Ensure fast and reliable charging with our high-quality USB-C cable.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Supports fast data transfer.</li>\r\n<li>Durable braided design.</li>\r\n<li>Reinforced connectors for long-lasting use.</li>\r\n<li>Available in various lengths.</li>\r\n</ul>', 5.99, 0, 40, 'cable.jpg', '2024-03-27 13:28:09', 'Vincent'),
-(7, 'Car Phone Holder Mount', '<p>Keep your phone secure and accessible while driving with our convenient car phone holder mount.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>360-degree rotation for flexible viewing angles.</li>\r\n<li>Strong suction cup for stable attachment.</li>\r\n<li>Adjustable arm to fit different devices.</li>\r\n<li>Easy installation and removal.</li>\r\n</ul>', 19.99, 0, 40, 'phoneholder.jpg', '2024-03-27 13:32:33', 'Vincent'),
-(8, 'Wireless Charger', '<p>Experience the future of charging with our efficient wireless charger.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Fast and convenient charging without cables.</li>\r\n<li>Universal compatibility with Qi-enabled devices.</li>\r\n<li>LED indicator light for charging status.</li>\r\n<li>Sleek and compact design.</li>\r\n</ul>', 99.99, 0, 25, 'charger.jpg', '2024-03-27 13:40:46', 'Vincent');
+(6, 'USB-C Cable', '<p>Ensure fast and reliable charging with our high-quality USB-C cable.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Supports fast data transfer.</li>\r\n<li>Durable braided design.</li>\r\n<li>Reinforced connectors for long-lasting use.</li>\r\n<li>Available in various lengths.</li>\r\n</ul>', 5.99, 0.00, 39, 'cable.jpg', '2024-03-27 13:28:09', 'Vincent'),
+(7, 'Car Phone Holder Mount', '<p>Keep your phone secure and accessible while driving with our convenient car phone holder mount.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>360-degree rotation for flexible viewing angles.</li>\r\n<li>Strong suction cup for stable attachment.</li>\r\n<li>Adjustable arm to fit different devices.</li>\r\n<li>Easy installation and removal.</li>\r\n</ul>', 19.99, 0.00, 40, 'phoneholder.jpg', '2024-03-27 13:32:33', 'Vincent'),
+(8, 'Wireless Charger', '<p>Experience the future of charging with our efficient wireless charger.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Fast and convenient charging without cables.</li>\r\n<li>Universal compatibility with Qi-enabled devices.</li>\r\n<li>LED indicator light for charging status.</li>\r\n<li>Sleek and compact design.</li>\r\n</ul>', 99.99, 0.00, 20, 'charger.jpg', '2024-03-27 13:40:46', 'Vincent');
 
 -- --------------------------------------------------------
 
@@ -122,7 +122,7 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`staff_id`, `staff_name`, `staff_email`, `password`, `reg_date`) VALUES
-(1, 'Vincent', 'Vincent@email.com', 'e10adc3949ba59abbe56e057f20f883e', '2024-03-19 15:42:13');
+(1, 'Seven', 'Seven@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2024-07-11 09:38:42');
 
 --
 -- Indexes for dumped tables
@@ -168,7 +168,7 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `payment_method`
@@ -180,7 +180,7 @@ ALTER TABLE `payment_method`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `staff`
